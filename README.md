a030f2e607421526fb27ef679dc513292a596494
unknown <mtgalla@gmail.com>
2019-02-11 12:53:57 -0600

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Psychic-Game
+To play the Psychic-Game, from any letter a-z, try to guess what the computer chose. You get 9 tries to guess same letter as computer, if you guess the correct letter then you win, otherwise you lose.
+Your Wins and Losses will be tallied on the page along with the number of guesses remaining for each round.
+Good Luck!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,30 +15,41 @@
       // Randomly chooses a choice from the computer choice string. This is the Computer's guess.
       var computerGuess = choices[Math.floor(Math.random() * choices.length)]; 
 
-      // Key pressed assigned to output to html span.
+      // Key pressed assigned to output to HTML id.
       var userGuess = event.key;
       var divParent = document.getElementById("userGuess");
       var newDiv = document.createElement("span");
       
-      //
+      //Win if user gueses computer's choice and resets guesses
       if(computerGuess===userGuess){
         wins++;
         reset()
       }
-      
+      //Loss if user does not guess computer's choice after 9 guesses and resets guesses 
+      //Losses get incremented after last guess (i.e. guesses===1) 
       else {
       if (guesses===1){
         losses++;
         reset()
       }
+      //First incorrect guess (i.e. guesses === 9), outputs to userGuess HTML id with no comma 
       else{
+        if (guesses===9){
+          guesses--;
+          newDiv.textContent = (userGuess);
+          divParent.appendChild(newDiv);
+        }
+
+        //Remaining incorrect guesses, outputs to userGuess HTML id with leading comma
+        else{
         guesses--;
-        newDiv.textContent = (userGuess +" ")
+        newDiv.textContent = (", " + userGuess);
         divParent.appendChild(newDiv); 
+        }
       }
       }
 
-      //Output count of wins, loses and guesses to html span
+      //Output count of wins, loses and guesses to HTML respective ids
       var winParent = document.getElementById("winCount");
       winParent.textContent = (wins);
       
@@ -48,7 +59,8 @@
       var guessParent = document.getElementById("guessCount");
       guessParent.textContent = (guesses);
 
-      
+      //Left these console.log entries as comments. These helped me see results of the different variables 
+      //This was very helpful in trying to understand how the different methods and properies worked and helped me debug many of the errors
       //console.log(computerGuess);
       //console.log(allChoices);
       //console.log("losses=" + losses);
@@ -57,7 +69,7 @@
       //console.log(guesses);
       //console.log(newDiv);
 
-      //rest funtction that resets starting guesses to 9 and clears out user guesses from output html span.
+      //reset funtction that resets starting guesses to 9 and clears out user guesses from output html span.
       function reset(){
         guesses = 9;
         divParent.textContent = ("");
